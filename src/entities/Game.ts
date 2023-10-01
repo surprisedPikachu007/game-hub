f9b1d84c8679e64069fecd9372f22fb5a935d903
surprisedPikachu69 <arvind7447@gmail.com>
2023-10-01 11:22:20 +0530

    game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,8 +1,12 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[];
   name: string;
   description_raw: string;
   background_image: string;
